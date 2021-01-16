2ca0bfb82c64a28c5fe4c304a2c39606a98e7658
Levitsky Oleg <levitsky90@gmail.com>
2021-01-16 17:44:35 +0200

    Initial commit

---
+++ Shard.Core/UserInput/Keys.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Keys.cs" company="Oleg Levitsky">
+// Copyright (c) Oleg Levitsky. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Collections.Generic;
+
+namespace Shard.Core.UserInput
+{
+  /// <summary>
+  /// Provides a sequence of the <see cref="Key"/>.
+  /// </summary>
+  public class Keys : List<Key>
+  {
+  }
+}
---
+++ Shard.Interfaces/UserInterface/IClickable.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IClickable.cs" company="Oleg Levitsky">
+// Copyright (c) Oleg Levitsky. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Shard.Interfaces
+{
+  /// <summary>
+  /// Provides element that can handle a mouse click.
+  /// </summary>
+  public interface IClickable
+  {
+    /// <summary>
+    /// Invokes click.
+    /// </summary>
+    void Click();
+  }
+}
