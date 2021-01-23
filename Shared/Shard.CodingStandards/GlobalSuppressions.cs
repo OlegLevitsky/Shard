dca2e69f1abf0b2b78488adf41ae3ea945cae153
Levitsky Oleg <olevitsky90@gmail.com>
2021-01-23 21:31:21 +0200

    Add .editorconfig Turn on documentation generation

---
+++ Shared/Shard.CodingStandards/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Oleg Levitsky">
+// Copyright (c) Oleg Levitsky. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "Usings must be placed before namespace")]
